c0d1df2a276469b815c049d081c2b931f8f8601d
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-05-15 17:12:19 +0500

    Create some types

---
+++ streakmark-client/src/components/Board/Board.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  year: Streakmark.Year;
+  feeds: Streakmark.Feed[];
+}
+
+
+function Board(props: Props): JSX.Element {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Board;
---
+++ streakmark-client/src/types/types.d.ts
@@ -0,0 +1,20 @@
+declare namespace Streakmark {
+  interface Feed {
+    id: string;
+    name: string;
+    description: string;
+    entries: YearlyEntries;
+  }
+
+  type Entry = string;
+
+  type Day = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31;
+  type Month = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December';
+  type Year = number;
+
+  type DailyEntries = Record<Day, Entry[]>;
+
+  type MonthlyEntries = Record<Month, DailyEntries>;
+
+  type YearlyEntries = Record<Year, MonthlyEntries>;
+}
