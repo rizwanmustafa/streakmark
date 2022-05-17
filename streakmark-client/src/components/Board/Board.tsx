72760b22023cc3ea44a2e594f1eb46d1a5374df1
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-05-17 12:15:10 +0500

    Add board rendering random numbers

--- streakmark-client/src/components/Board/Board.tsx
+++ streakmark-client/src/components/Board/Board.tsx
@@ -1,13 +1,45 @@
+import "./Board.css";
 interface Props {
   year: Streakmark.Year;
   feeds: Streakmark.Feed[];
 }
 
+const colors = ["#161b22", "#0e4429", "#006d32", "#26a641", "#39d353"]
+
+function getRandomBoxStyle(): React.CSSProperties {
+  return {
+    backgroundColor: colors[Math.floor(Math.random() * 5)],
+    color: "transparent",
+    margin: 2,
+    fontFamily: "monospace",
+    width:12,
+    height:12,
+    borderRadius: 50,
+    userSelect: "none"
+  };
+}
+
 
 function Board(props: Props): JSX.Element {
-  return (
-    <div>
+  const myWeeklyElements = [];
 
+  for (let i = 0; i <= 52; i++) {
+    myWeeklyElements.push(
+      <div className="week">
+        <div className="day" style={getRandomBoxStyle()}>&nbsp</div>
+        <div className="day" style={getRandomBoxStyle()}>&nbsp</div>
+        <div className="day" style={getRandomBoxStyle()}>&nbsp</div>
+        <div className="day" style={getRandomBoxStyle()}>&nbsp</div>
+        <div className="day" style={getRandomBoxStyle()}>&nbsp</div>
+        <div className="day" style={getRandomBoxStyle()}>&nbsp</div>
+        <div className="day" style={getRandomBoxStyle()}>&nbsp</div>
+
+      </div>
+    )
+  }
+  return (
+    <div id="board">
+      {myWeeklyElements}
     </div>
   );
 }
