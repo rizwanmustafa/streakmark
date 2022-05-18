371606ad101dcf01867c40f2e2f707cdfeec0ea4
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-05-18 10:41:01 +0500

    Add test data for week component

--- streakmark-client/src/App.tsx
+++ streakmark-client/src/App.tsx
@@ -1,12 +1,30 @@
 import Board from "./components/Board/Board";
-import Day from "./components/Day/Day";
+import Week from "./components/Week/Week";
 
 function App(): JSX.Element {
   return (
-    <div style={{ padding: 50, border: "1px solid white", borderRadius:25, display: "flex", flexDirection:"column", alignItems:"center"}}>
-      <h1 style={{ color: "white", fontFamily:"Roboto, sans-serif"}}>Programming</h1>
+    <div style={{ padding: 50, border: "1px solid white", borderRadius: 25, display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h1 style={{ color: "white", fontFamily: "Roboto, sans-serif" }}>Programming</h1>
       <Board year={2021} feeds={[]} />
-      <Day borderRadius={50} color="#161b22" hoverText="Monday" />
+      <Week
+      borderRadius={1}
+        year={2021}
+        weekNumber={2}
+        average={5}
+        highest={10}
+        lowest={0}
+        dailyData={
+          {
+            0: ["asdfasdf", "asdfasdf"],
+            1: ["asdfasdf", "asdfasdf", "asdfasdf", "asdfasd", "asdfasdf"],
+            2: ["asdfasdf", "asdfasdf", "asdfas", "asdfas", "asdf", "asdfasdf", "asdfasdf", "asdf", "asdf", "asdf"],
+            3: [],
+            4: ["asdfasdf", "asdfasdf", "asdfasdf", "asdfas", "asdfas", "asdfasd"],
+            5: ["asdfasdf"],
+            6: ["asdfasdf", "asdfasd", "asdfasd"]
+          }
+        }
+      />
     </div>
   )
 }
