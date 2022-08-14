10fe73c225d1d468faf73a126654078159e868b2
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-08-14 11:16:14 +0500

    Create types for server

---
+++ streakmark-server/src/types/types.d.ts
@@ -0,0 +1,29 @@
+import { ObjectId } from "mongodb";
+
+declare namespace StreakMarkServer {
+
+  interface User {
+    uid: string;
+    username: string;
+    email: string;
+    hashedPassword: string;
+    creationDate: string;
+  }
+
+  interface Feed {
+    title: string;
+    description: string;
+  }
+
+  interface Task {
+    title: string;
+    completionDate: string;
+    feedId: ObjectId;
+  }
+
+  interface Board {
+    title: string;
+    description: string;
+    feedIds: ObjectId[];
+  }
+}
