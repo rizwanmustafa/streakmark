ff33caf1e7802987678e3407f825d96af5401335
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-08-19 12:01:20 +0500

    Add getFeeds and updateFeeds dal functions

--- streakmark-server/src/dal/feeds.ts
+++ streakmark-server/src/dal/feeds.ts
@@ -26,6 +26,54 @@ export async function addFeed(uid: string, feed: StreakMarkServer.Feed): Promise
   });
 }
 
+export async function getFeeds(uid: string, feedId: string | null): Promise<StreakMarkServer.Feed[]> {
+  const tasks = getCollection<StreakMarkServer.Feed>("feeds");
+
+  const query: { uid: string, _id?: ObjectId } = {
+    uid: uid,
+  };
+
+  if (feedId) {
+    query._id = new ObjectId(feedId);
+  }
+  const cursor = tasks.find(query);
+  const feedArray = await cursor.toArray();
+
+  return feedArray;
+}
+
+export async function updateFeed(uid: string, feedId: string, newFeed: StreakMarkServer.Feed): Promise<void> {
+  const user = await getUserWithUid(uid);
+  if (!user) {
+    Logger.error(`User with uid ${uid} does not exist`);
+    return;
+  }
+
+  const feedCollection = getCollection<StreakMarkServer.Feed>("feeds");
+  const feedObjectId = new ObjectId(feedId);
+  const feed = await feedCollection.findOne({ _id: feedObjectId });
+
+  if (!feed) {
+    Logger.error(`Feed with id ${feedId} does not exist`);
+    return;
+  }
+
+  if (feed.uid !== uid) {
+    Logger.error(`User does not own feed!`);
+    return;
+  }
+
+  const query = {
+    uid: uid,
+    _id: feedObjectId
+  };
+  const update = {
+    $set: newFeed,
+  };
+  await feedCollection.updateOne(query, update);
+
+}
+
 export async function removeFeed(uid: string, feedId: string): Promise<void> {
   const user = getUserWithUid(uid);
 
